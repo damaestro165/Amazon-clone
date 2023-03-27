51c8f165a944e5680ce7069556497cf2c44c6193
damaestro165 <damaestro165@gmail.com>
2023-03-27 15:45:39 +0100

    Feat: Bottom Bar

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,3 +1,29 @@
+import { BiMenu } from 'react-icons/bi';
+import { Icon, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+
 export const BottomBar = () => {
-  return <div>BottomBar</div>;
+  return (
+    <div className='bg-[#232f3e] w-full h-8 flex p-2 items-center text-white text-xs gap-4 font-semibold'>
+      <div className='text-white flex items-center'>
+        <Icon as={BiMenu} color='white' />
+        <Text>All</Text>
+      </div>
+      <Link href='/' legacyBehavior>
+        <a>Today&apos;s Deal</a>
+      </Link>
+      <Link href='/' legacyBehavior>
+        <a>Customer Service</a>
+      </Link>
+      <Link href='/' legacyBehavior>
+        <a>Registry</a>
+      </Link>
+      <Link href='/' legacyBehavior>
+        <a>Gift Cards</a>
+      </Link>
+      <Link href='/' legacyBehavior>
+        <a>Sell</a>
+      </Link>
+    </div>
+  );
 };
