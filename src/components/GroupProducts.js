65d51f305abb0027759826f1df0e26e884e66c3c
damaestro165 <damaestro165@gmail.com>
2023-03-31 15:53:51 +0100

    feat: Dynamic product route

--- src/components/GroupProducts.js
+++ src/components/GroupProducts.js
@@ -3,7 +3,26 @@ import ProductCard from './ProductCard';
 export const GroupProducts = () => {
   return (
     <div className=' z-50 grid gap-3 grid-cols-4 w-[90%]'>
-      <ProductCard title='jewelery' image='/images/jewelry.jpeg' />
+      <ProductCard
+        title='Jewelery'
+        image='/images/jewelry.jpeg'
+        query='jewelry'
+      />
+      <ProductCard
+        title='Electronics'
+        image='/images/jewelry.jpeg'
+        query='electronics'
+      />
+      <ProductCard
+        title="Women's cloth"
+        image='/images/jewelry.jpeg'
+        query="men's%20clothing"
+      />
+      <ProductCard
+        title="Men's cloth"
+        image='/images/jewelry.jpeg'
+        query="men's%20clothing"
+      />
     </div>
   );
 };
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,16 +2,21 @@ import { Heading, VStack, Text, Box } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const ProductCard = ({ title, image }) => {
+const ProductCard = ({ title, image, query }) => {
   return (
     <>
       <Box className='flex flex-col items-start bg-white p-5 gap-5'>
         <Heading as='h4' fontSize='xl' fontWeight='semibold'>
           {title}
         </Heading>
-        <Link href={`/categories/${title}`}>
+        <Link href={`/categories/${query}`}>
           <Image src={image} alt='productImage' width={250} height={250} />
         </Link>
+        <Box>
+          <Link href={`/categories/${query}`} legacyBehavior>
+            <a>See more</a>
+          </Link>
+        </Box>
       </Box>
     </>
   );
