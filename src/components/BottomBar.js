871b58d73706d427ae08f86d0c1605f68baea00e
damaestro165 <damaestro165@gmail.com>
2023-05-06 22:01:53 +0100

    chore{drawer}

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,13 +1,16 @@
 import { BiMenu } from 'react-icons/bi';
 import { Icon, Text } from '@chakra-ui/react';
 import Link from 'next/link';
+import SideBar from './SideBar';
 
 export const BottomBar = () => {
   return (
     <div className='bg-[#232f3e] w-full h-8 flex p-2 items-center text-white text-xs gap-4 font-semibold'>
       <div className='text-white flex items-center'>
-        <Icon as={BiMenu} color='white' />
-        <Text>All</Text>
+        <SideBar>
+          <Icon as={BiMenu} color='white' />
+          <Text>All</Text>
+        </SideBar>
       </div>
       <Link href='/' legacyBehavior>
         <a>Today&apos;s Deal</a>
---
+++ src/components/SideBar.js
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+import React from 'react';
+
+const SideBar = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  return (
+    <>
+      <Box ref={btnRef} colorScheme='teal' onClick={onOpen} className='flex'>
+        {children}
+      </Box>
+      <Drawer
+        isOpen={isOpen}
+        placement='left'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody></DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default SideBar;
